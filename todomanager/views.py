c9832f52e021799ec2b793458a81ef34968f4688
neoinal <colt.endless.try@gmail.com>
2015-12-13 14:01:33 +0900

    ToDoの一覧と編集機能の作成

--- todomanager/views.py
+++ todomanager/views.py
@@ -45,3 +45,42 @@ class TaskView(object):
                 raise ValueError("CSRF token did not match.")
         return parameter
 
+    @view_config(route_name='list', renderer='templates/list.jinja2')
+    def list(self):
+        tasks = DBSession.query(Task).all()
+        return {
+            "page_title": "ToDo Manager - タスクの一覧",
+            "active_page": "list",
+            "tasks": tasks,
+        }
+
+    @view_config(route_name='edit', renderer='templates/edit.jinja2')
+    def edit(self):
+        parameter = {
+            "page_title": "ToDo Manager - タスクの編集",
+            "active_page": "",
+        }
+        task_id = int(self.request.matchdict["task_id"])
+        task = DBSession.query(Task).get(task_id)
+        if self.request.method == 'POST':
+            if check_csrf_token(self.request):
+                summary = self.request.POST.get("summary")
+                if summary is not None:
+                    task.summary = summary
+                    DBSession.add(task)
+                    result = {
+                        "status": "success",
+                        "message": "ToDoの更新に成功しました。",
+                    }
+                else:
+                    result = {
+                        "status": "error",
+                        "message": "内容が入力されていません。",
+                    }
+
+                parameter["result"] = result
+            else:
+                raise ValueError("CSRF token did not match.")
+
+        parameter["task"] = task
+        return parameter
